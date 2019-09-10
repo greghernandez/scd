f84810c78aacba7a4d38984da77563b147892067
greghernandez <gregoryarthur13@gmail.com>
2019-09-10 15:18:40 -0500

    Integración de Apollo-Vue

---
+++ src/boot/vue-apollo.js
@@ -0,0 +1,28 @@
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'https://countries.trevorblades.com/'
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
+
+export default ({ app, Vue }) => {
+  Vue.use(VueApollo)
+  app.apolloProvider = apolloProvider
+}
---
+++ src/pages/Doc-pendientes.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>Graph</h2>
+        <div v-for="countries in countries" :key="countries.id" >
+            <p>{{ countries.code }} - {{ countries.name }} - {{ countries.emoji }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  apollo: {
+    countries: gql`query {
+        countries{
+            code,
+            name,
+            emoji
+        }
+    }`
+  },
+  data () {
+    return {
+      countries: []
+    }
+  }
+}
+</script>
