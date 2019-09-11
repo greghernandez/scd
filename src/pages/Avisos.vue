67d47c2c88040593bd80144765d60b51a62e7eb5
greghernandez <gregoryarthur13@gmail.com>
2019-09-11 18:53:42 -0500

    Update Avisos.vue

---
+++ src/pages/Avisos.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>
+      <h3>Avisos</h3>
+    </div>
+    <div>
+      <q-btn unelevated rounded dense color="primary" label="Crear aviso" no-caps @click="crearAviso()"/>
+    </div>
+    <div class="q-mt-lg">
+      <TablaAvisos />
+    </div>
+  </div>
+</template>
+
+<script>
+import TablaAvisos from 'components/avisos/TablaAvisos'
+import ModalCrearAviso from 'components/avisos/CrearAviso'
+
+export default {
+  name: 'PageAvisos',
+  components: {
+    TablaAvisos
+  },
+  methods: {
+    crearAviso () {
+      this.$q.dialog({
+        component: ModalCrearAviso
+      })
+    }
+  }
+}
+</script>
