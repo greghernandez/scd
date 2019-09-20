18e88c1566ad4e94795394e1f585df8e7b334172
greghernandez <gregoryarthur13@gmail.com>
2019-09-20 13:50:50 -0500

    Create InfoCard.vue

---
+++ src/components/inicio/InfoCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-card class="my-card info-content">
+    <q-card-section>
+      <div class="text-center">
+        <div>
+          <q-avatar class="secondary-spc" size="lg" text-color="secondary" icon="eva-file-outline" />
+        </div>
+        <div>
+          <h4 class="q-my-none">5</h4>
+        </div>
+        <div class="label-text">
+          Documentos
+        </div>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'InfoCard'
+}
+</script>
