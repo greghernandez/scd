2dcc987277eb59b97710d1f6c89d01fa145c85f7
greghernandez <gregoryarthur13@gmail.com>
2019-09-20 13:50:43 -0500

    Create BoardAvisos.vue

---
+++ src/components/inicio/BoardAvisos.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-card class="my-card-outlined flat">
+    <q-card-section>
+      <div class="row ">
+        <q-icon name="eva-alert-triangle-outline" />
+        <h5 class="q-my-none">Avisos</h5>
+      </div>
+      <!-- cards con los avisos -->
+      <div>
+        <q-card v-for="n in 7" :key="n" class="my-card q-my-sm">
+          <q-card-secction>
+            <div>
+            </div>
+            <div>Titulo</div>
+            <div>Fecha</div>
+            <div>Contenido</div>
+            <q-btn outline rounded dense color="primary" label="Ver más" no-caps />
+          </q-card-secction>
+        </q-card>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'BoardAvisos'
+}
+</script>
