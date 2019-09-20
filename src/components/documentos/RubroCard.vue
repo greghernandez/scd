dead9cf03db3355a9d5e7e59d6b7a63c633172eb
greghernandez <gregoryarthur13@gmail.com>
2019-09-20 13:50:26 -0500

    Create RubroCard.vue

---
+++ src/components/documentos/RubroCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-card class="my-card rubro-content row justify-center flex-center">
+    <q-card-section class="row justify-center items-center content-center">
+      <div class="col full-with">
+        <q-avatar color="secondary" text-color="white">01</q-avatar>
+      </div>
+      <div class="q-ml-sm q-mb-none" style="height: 100%">
+        <p>Estudios Especiales</p>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'RubroCard'
+}
+</script>
