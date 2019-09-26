e93a7fb4643b0c919d9e8fe1e21fcb8523ecad38
greghernandez <gregoryarthur13@gmail.com>
2019-09-26 18:16:07 -0500

    Update Administradores.vue

---
+++ src/pages/Administradores.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+      <h4 class="q-mt-md q-mb-md">Administradores</h4>
+    </div>
+    <div class="q-mt-lg">
+      <TablaAdministradores />
+    </div>
+  </div>
+</template>
+
+<script>
+import TablaAdministradores from 'components/administradores/TablaAdministradores'
+
+export default {
+  name: 'PageAdministradoress',
+  components: {
+    TablaAdministradores
+  },
+  methods: {
+  }
+}
+</script>
