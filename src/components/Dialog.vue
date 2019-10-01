ce34eb17dfe6def3a66ae28996e481c78d77a2e4
greghernandez <gregoryarthur13@gmail.com>
2019-10-01 18:01:58 -0500

    Update Dialog.vue

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,17 +1,19 @@
 <template>
-  <q-dialog ref="dialog" v-model="confirm" persistent>
-    <q-card class="my-modal">
-      <q-card-section>
-          <div class="text-h6">{{ titulo }}</div>
-        </q-card-section>
+  <q-dialog ref="dialog" v-model="alertAviso" persistent>
+    <q-card class="my-modal items-center">
+      <q-card-section class="vertical-middle">
+        <div class="row">
+          <div class="text-h6">{{ title }}</div>
+        </div>
+      </q-card-section>
 
         <q-card-section>
-          {{lorem}}
+          {{ message }}
         </q-card-section>
 
       <q-card-actions align="right">
-        <q-btn flat label="Cancel" color="primary" v-close-popup @click="onCancelClick()" />
-        <q-btn flat label="Turn on Wifi" color="primary" v-close-popup @click="onOKClick()" />
+        <q-btn rounded unelevated outline dense label="Cancelar" color="" v-close-popup @click="onCancelClick()" no-caps />
+        <q-btn rounded unelevated outline dense :label="btn" :color="btnColor" v-close-popup @click="onOKClick()" no-caps />
       </q-card-actions>
     </q-card>
   </q-dialog>
@@ -19,46 +21,37 @@
 
 <script>
 export default {
-  name: 'Modal',
+  name: 'AlertAvisos',
   data () {
     return {
-      titulo: 'Inserte un titulo',
-      lorem: 'hola',
-      confirm: false
+      alertAviso: false
     }
   },
+  props: {
+    title: String,
+    message: String,
+    btn: String,
+    btnColor: String
+  },
   methods: {
-    // following method is REQUIRED
-    // (don't change its name --> "show")
     show () {
       this.$refs.dialog.show()
     },
-
-    // following method is REQUIRED
-    // (don't change its name --> "hide")
     hide () {
       this.$refs.dialog.hide()
     },
-
     onDialogHide () {
       // required to be emitted
       // when QDialog emits "hide" event
       this.$emit('hide')
     },
-
     onOKClick () {
       console.log('Ok')
-      // on OK, it is REQUIRED to
-      // emit "ok" event (with optional payload)
-      // before hiding the QDialog
       this.$emit('ok')
-      // or with payload: this.$emit('ok', { ... })
-      // then hiding dialog
       this.hide()
     },
     onCancelClick () {
       console.log('Cancel')
-      // we just need to hide dialog
       this.hide()
     }
   }
