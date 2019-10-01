0d2e40d0ef92c61b984eeba2b9d399cba2966014
greghernandez <gregoryarthur13@gmail.com>
2019-10-01 17:57:18 -0500

    Create Login.vue

---
+++ src/pages/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="absolute-center">
+        <q-card class=" my-card">
+            <q-card-section>
+                <div>
+                    <q-img src=""/>
+                </div>
+                <h5>Inicio de sesión</h5>
+            </q-card-section>
+        </q-card>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
