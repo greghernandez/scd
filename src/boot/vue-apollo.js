2493e5ebef1bb2856ba19ee9e994c464d6556f35
greghernandez <gregoryarthur13@gmail.com>
2019-10-11 12:42:10 -0500

    Update vue-apollo.js

--- src/boot/vue-apollo.js
+++ src/boot/vue-apollo.js
@@ -2,12 +2,13 @@ import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
+import { typeDefs } from '../services/graphql/resolvers'
 
 // HTTP connection to the API
 const httpLink = createHttpLink({
   // You should use an absolute URL here
   //  uri: 'https://countries.trevorblades.com/'
-  uri: 'http://148.220.209.251:4000/graphql'
+  uri: 'http://148.220.210.76:4000/graphql'
 })
 
 // Cache implementation
@@ -16,7 +17,9 @@ const cache = new InMemoryCache()
 // Create the apollo client
 const apolloClient = new ApolloClient({
   link: httpLink,
-  cache
+  cache,
+  typeDefs,
+  resolvers: {}
 })
 
 const apolloProvider = new VueApollo({
