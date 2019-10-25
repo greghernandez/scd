887242fe6538645d00d918713da54a13f22d4b6b
greghernandez <gregoryarthur13@gmail.com>
2019-10-25 13:23:22 -0500

    Update strcuture and get data

--- src/components/documentos/documentoLabel.vue
+++ src/components/documentos/documentoLabel.vue
@@ -1,36 +1,38 @@
 <template>
   <div>
     <q-card class="doc-card">
-      <q-card-section class="q-py-xs q-px-md">
+      <q-card-section class="q-py-xs q-px-xs">
         <div class="row justify-between items-center content-center">
-            <div class="column">
-              <div class="col-12">
-                <q-icon color="negative" name="eva-file-text" style="font-size: 2rem;" />
+            <div class="row justify-between items-center content-center">
+              <div class="col-2">
+                <div class="col-12">
+                  <q-icon color="negative" name="eva-file-text" style="font-size: 2rem;" />
+                </div>
               </div>
-            </div>
-            <div class="column text-caption">
-              <div class="col-6 text-weight-bold">{{ fileName }}</div>
-              <div class="col-6">{{ createdAt }}</div>
-            </div>
-            <div class="column">
-              <q-btn round flat icon="eva-more-vertical-outline">
-                <q-menu transition-show="scale" transition-hide="scale" anchor="center right" self="center right">
-                  <q-list style="min-width: 100px">
-                    <q-item clickable>
-                      <q-item-section>Abrir</q-item-section>
-                    </q-item>
-                    <q-item clickable>
-                      <q-item-section @click="mover()" >Mover a</q-item-section>
-                    </q-item>
-                    <q-item clickable>
-                      <q-item-section>Descargar</q-item-section>
-                    </q-item>
-                    <q-item clickable>
-                      <q-item-section @click="eliminar()">Eliminar</q-item-section>
-                    </q-item>
-                  </q-list>
-                </q-menu>
-              </q-btn>
+              <div class="col-8 text-caption">
+                <div class="col text-weight-bold text-inline">{{ fileName }}</div>
+                <div class="col">{{ createdAt }}</div>
+              </div>
+              <div class="col-2">
+                <q-btn round flat icon="eva-more-vertical-outline">
+                  <q-menu transition-show="scale" transition-hide="scale" anchor="center right" self="center right">
+                    <q-list style="min-width: 100px">
+                      <q-item clickable>
+                        <q-item-section>Abrir</q-item-section>
+                      </q-item>
+                      <q-item clickable>
+                        <q-item-section @click="mover()" >Mover a</q-item-section>
+                      </q-item>
+                      <q-item clickable>
+                        <q-item-section>Descargar</q-item-section>
+                      </q-item>
+                      <q-item clickable>
+                        <q-item-section @click="eliminar()">Eliminar</q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-menu>
+                </q-btn>
+              </div>
             </div>
         </div>
       </q-card-section>
