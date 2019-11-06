5bef5dd418eb1defa43c572f8415da4865cd6fe8
greghernandez <gregoryarthur13@gmail.com>
2019-11-06 12:06:41 -0600

    added dynamic user id

--- src/pages/Doc-pendientes.vue
+++ src/pages/Doc-pendientes.vue
@@ -41,7 +41,7 @@ export default {
       query: documentsTartaro,
       variables: {
         search: {
-          user: '5dadf9dfd0cd0a1031b3652e',
+          user: payload.userId,
           page: 0,
           perPage: 0,
           category: '5db0cad2c7baf510c480a7e0'
