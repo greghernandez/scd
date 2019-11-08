4fcfdda003aa5d825f47d41f79d0c77c2d253e9a
John Reb <john.reb97@gmail.com>
2019-11-08 13:29:31 -0600

    Update mutations.js

--- src/services/graphql/mutations.js
+++ src/services/graphql/mutations.js
@@ -12,7 +12,7 @@ export const noticeCreateMutation = gql`mutation CreateNotice($file: Upload!, $i
 }
 `
 // notice Mutation Update
-export const noticeUpdateMutation = gql`mutation UpdateNotice($id: ID!, $input: UpdateNotice, $file: Upload){
+export const noticeUpdateMutation = gql`mutation UpdateNotice($id: ID!, $input: UpdateNotice!, $file: Upload){
   updateNotice(id: $id, input: $input, file: $file)
   {
     _id
