89f4295d631014126d13310fc5a92dabd80205d8
GregHernandez <gregoryarthur13@gmail.com>
2020-01-07 09:24:50 -0600

    Update documentoLabel.vue

--- src/components/documentos/documentoLabel.vue
+++ src/components/documentos/documentoLabel.vue
@@ -41,43 +41,6 @@
             </q-btn>
           </div>
         </div>
-        <!--
-        <div class="row justify-between items-center content-center">
-          <div class="row justify-between items-center content-center">
-            <div class="col-2">
-              <div class="col-12">
-                <q-icon color="negative" name="eva-file-text" style="font-size: 2rem;" />
-              </div>
-            </div>
-            <div class="col-8 text-caption">
-              <q-tooltip>
-                {{ fileName }}
-              </q-tooltip>
-              <div class="col text-weight-bold text-inline">{{ fileName }}</div>
-              <div class="col">{{ createdAt }}</div>
-            </div>
-            <div class="col-2">
-              <q-btn round flat icon="eva-more-vertical-outline">
-                <q-menu transition-show="scale" transition-hide="scale" anchor="center right" self="center right">
-                  <q-list style="min-width: 100px">
-                    <q-item clickable>
-                      <q-item-section @click="abrir()">Abrir</q-item-section>
-                    </q-item>
-                    <q-item clickable>
-                      <q-item-section @click="mover()">Mover a</q-item-section>
-                    </q-item>
-                    <q-item clickable>
-                      <q-item-section @click="descargar()">Descargar</q-item-section>
-                    </q-item>
-                    <q-item clickable>
-                      <q-item-section @click="eliminar()">Eliminar</q-item-section>
-                    </q-item>
-                  </q-list>
-                </q-menu>
-              </q-btn>
-            </div>
-          </div>
-        </div>-->
       </q-card-section>
     </q-card>
   </div>
