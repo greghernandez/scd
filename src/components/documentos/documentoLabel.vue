6c21267a7cc9b6b7ca37c2e8530058047d5e9c01
GregHernandez <gregoryarthur13@gmail.com>
2020-01-07 09:10:41 -0600

    Update documentoLabel structure

--- src/components/documentos/documentoLabel.vue
+++ src/components/documentos/documentoLabel.vue
@@ -2,42 +2,82 @@
   <div>
     <q-card class="doc-card">
       <q-card-section class="q-py-xs q-px-xs">
+        <div class="row col-12 justify-between items-center content-center">
+          <div class="col-1">
+            <q-checkbox dense v-model="val" :val="objId"/>
+          </div>
+          <div class="col-2">
+            <q-icon color="negative" name="eva-file-text" style="font-size: 2rem;" />
+          </div>
+          <div class="col-7">
+            <q-tooltip>
+              {{ fileName }}
+            </q-tooltip>
+            <div class="ellipsis text-body2">
+              {{ fileName }}
+            </div>
+            <div class="text-caption">
+              {{ createdAt | dateFormat }}
+            </div>
+          </div>
+          <div class="col-2">
+            <q-btn round flat icon="eva-more-vertical-outline">
+              <q-menu transition-show="scale" transition-hide="scale" anchor="center right" self="center right">
+                <q-list style="min-width: 100px">
+                  <q-item clickable>
+                    <q-item-section @click="abrir()">Abrir</q-item-section>
+                  </q-item>
+                  <q-item clickable>
+                    <q-item-section @click="mover()">Mover a</q-item-section>
+                  </q-item>
+                  <q-item clickable>
+                    <q-item-section @click="descargar()">Descargar</q-item-section>
+                  </q-item>
+                  <q-item clickable>
+                    <q-item-section @click="eliminar()">Eliminar</q-item-section>
+                  </q-item>
+                </q-list>
+              </q-menu>
+            </q-btn>
+          </div>
+        </div>
+        <!--
         <div class="row justify-between items-center content-center">
-            <div class="row justify-between items-center content-center">
-              <div class="col-2">
-                <div class="col-12">
-                  <q-icon color="negative" name="eva-file-text" style="font-size: 2rem;" />
-                </div>
-              </div>
-              <div class="col-8 text-caption">
-                <q-tooltip>
-                  {{ fileName }}
-                </q-tooltip>
-                <div class="col text-weight-bold text-inline">{{ fileName }}</div>
-                <div class="col">{{ createdAt }}</div>
-              </div>
-              <div class="col-2">
-                <q-btn round flat icon="eva-more-vertical-outline">
-                  <q-menu transition-show="scale" transition-hide="scale" anchor="center right" self="center right">
-                    <q-list style="min-width: 100px">
-                      <q-item clickable>
-                        <q-item-section @click="abrir()">Abrir</q-item-section>
-                      </q-item>
-                      <q-item clickable>
-                        <q-item-section @click="mover()" >Mover a</q-item-section>
-                      </q-item>
-                      <q-item clickable>
-                        <q-item-section @click="descargar()">Descargar</q-item-section>
-                      </q-item>
-                      <q-item clickable>
-                        <q-item-section @click="eliminar()">Eliminar</q-item-section>
-                      </q-item>
-                    </q-list>
-                  </q-menu>
-                </q-btn>
+          <div class="row justify-between items-center content-center">
+            <div class="col-2">
+              <div class="col-12">
+                <q-icon color="negative" name="eva-file-text" style="font-size: 2rem;" />
               </div>
             </div>
-        </div>
+            <div class="col-8 text-caption">
+              <q-tooltip>
+                {{ fileName }}
+              </q-tooltip>
+              <div class="col text-weight-bold text-inline">{{ fileName }}</div>
+              <div class="col">{{ createdAt }}</div>
+            </div>
+            <div class="col-2">
+              <q-btn round flat icon="eva-more-vertical-outline">
+                <q-menu transition-show="scale" transition-hide="scale" anchor="center right" self="center right">
+                  <q-list style="min-width: 100px">
+                    <q-item clickable>
+                      <q-item-section @click="abrir()">Abrir</q-item-section>
+                    </q-item>
+                    <q-item clickable>
+                      <q-item-section @click="mover()">Mover a</q-item-section>
+                    </q-item>
+                    <q-item clickable>
+                      <q-item-section @click="descargar()">Descargar</q-item-section>
+                    </q-item>
+                    <q-item clickable>
+                      <q-item-section @click="eliminar()">Eliminar</q-item-section>
+                    </q-item>
+                  </q-list>
+                </q-menu>
+              </q-btn>
+            </div>
+          </div>
+        </div>-->
       </q-card-section>
     </q-card>
   </div>
@@ -48,6 +88,7 @@ import ModalMover from 'components/documentos/modalMover'
 import ModalEliminar from 'components/documentos/modalEliminar'
 import modalDocs from 'components/documentos/modalDocs'
 import { getFile } from '../../services/downloads'
+import { date } from 'quasar'
 
 export default {
   name: 'DocLabel',
@@ -56,7 +97,15 @@ export default {
   },
   data () {
     return {
-      pdfsrc: ''
+      pdfsrc: '',
+      val: false
+    }
+  },
+  filters: {
+    dateFormat: function (value) {
+      if (!value) return 'NA'
+      value = date.formatDate(value, 'DD-MM-YYYY')
+      return value
     }
   },
   props: {
