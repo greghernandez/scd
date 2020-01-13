e77af9ba0e939ff52436dbc7ee46942816bf9c4d
GregHernandez <gregoryarthur13@gmail.com>
2020-01-13 16:32:31 -0600

    Update login page
    - background
    - Structure

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,58 +1,59 @@
 <template>
-  <div class="absolute-center">
-    <div class="row justify-center items-center content-center">
-      <q-card class="my-card login-card text-center">
-        <q-card-section>
-          <div class="q-mt-sm">
-            <q-img class="logo-uaq" src="assets/logo-uaq.png" />
-          </div>
-          <h5 class="text-weight-medium text-primary q-my-sm">Inicio de sesión</h5>
-        </q-card-section>
-
-        <q-card-section class="full-width q-px-xl">
-          <q-form @submit="login" class="q-gutter-xs">
-            <div v-if="error">
-              <q-banner inline-actions class="text-white bg-red-7">
-                {{ errMsg }}
-              </q-banner>
+  <div class="full-width full-height">
+    <div class="bg-gradient">
+      <div class="absolute-center">
+        <q-card class="my-card login-card text-center">
+          <q-card-section>
+            <div class="q-mt-sm">
+              <q-img class="logo-uaq" src="assets/logo-uaq.png" />
             </div>
+            <h5 class="text-weight-medium text-primary q-my-sm">Inicio de sesión</h5>
+          </q-card-section>
 
-            <q-input dense rounded outlined  v-model="claveEmpleado" label="Clave de empleado" lazy-rules
-              :rules="[ val => val && val.length > 0 || 'Completa el campo']">
-              <template v-slot:prepend>
-                <q-icon name="eva-person-outline" />
-              </template>
-            </q-input>
+          <q-card-section class="full-width q-px-xl">
+            <q-form @submit="login" class="q-gutter-xs">
+              <div v-if="error">
+                <q-banner inline-actions class="text-white bg-red-7">
+                  Ocurrió un error, intentalo de nuevo
+                </q-banner>
+              </div>
 
-            <q-input dense rounded outlined class="q-pb-xs" type="password" v-model="password" label="Contraseña"
-              lazy-rules :rules="[ val => val && val.length > 0 || 'Completa el campo']">
-              <template v-slot:prepend>
-                <q-icon name="eva-lock-outline" />
-              </template>
-            </q-input>
+              <q-input dense rounded outlined  v-model="claveEmpleado" label="Clave de empleado" lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Completa el campo']">
+                <template v-slot:prepend>
+                  <q-icon name="eva-person-outline" />
+                </template>
+              </q-input>
 
-            <!--<div class="float-left q-mb-sm">
-              <q-checkbox v-model="recordarme" label="Recordarme" />
-            </div>-->
+              <q-input dense rounded outlined class="q-pb-xs" type="password" v-model="password" label="Contraseña"
+                lazy-rules :rules="[ val => val && val.length > 0 || 'Completa el campo']">
+                <template v-slot:prepend>
+                  <q-icon name="eva-lock-outline" />
+                </template>
+              </q-input>
 
+              <!--<div class="float-left q-mb-sm">
+                <q-checkbox v-model="recordarme" label="Recordarme" />
+              </div>-->
+              <div class="q-pt-md">
+                <q-btn rounded class="btn-login" label="Entrar" type="submit" color="primary" no-caps />
+              </div>
+            </q-form>
+          </q-card-section>
+
+          <q-card-section>
             <div>
-              <q-btn rounded class="btn-login" label="Entrar" type="submit" color="primary" no-caps />
+              <q-img class="logo-cd" src="assets/logo-cd.png" />
             </div>
-          </q-form>
-        </q-card-section>
+            Centro de desarrollo 2019
+          </q-card-section>
+        </q-card>
 
-        <q-card-section>
-          <div>
-            <q-img class="logo-cd" src="assets/logo-cd.png" />
-          </div>
-          Centro de desarrollo 2019
-        </q-card-section>
-      </q-card>
-
-      <!--<div v-for="(item, index) in loginData" :key="index">
-        <p>{{item.token}}</p>
-        <p>{{ item.message  }}</p>
-      </div>-->
+        <!--<div v-for="(item, index) in loginData" :key="index">
+          <p>{{item.token}}</p>
+          <p>{{ item.message  }}</p>
+        </div>-->
+      </div>
     </div>
   </div>
 </template>
