2ab01a82d79c374a50c7edd01202d384bcd0536b
GregHernandez <gregoryarthur13@gmail.com>
2020-01-15 13:34:55 -0600

    Update Menu.vue

--- src/components/documentos/floating-buttons/Menu.vue
+++ src/components/documentos/floating-buttons/Menu.vue
@@ -2,9 +2,9 @@
   <div>
     <q-page-sticky position="bottom-right" :offset="[18, 86]">
       <q-fab icon="eva-more-vertical-outline" direction="up" color="primary">
-        <q-fab-action @click="onClick" color="primary" icon="eva-move-outline" />
-        <q-fab-action @click="onClick" color="primary" icon="eva-cloud-download-outline" />
         <q-fab-action @click="onClick" color="negative" icon="eva-trash-outline" />
+        <q-fab-action @click="onClick" color="secondary" icon="eva-move-outline" />
+        <q-fab-action @click="onClick" color="primary" icon="eva-cloud-download-outline" />
       </q-fab>
     </q-page-sticky>
   </div>
