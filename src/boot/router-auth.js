6c59b6de0dbe6db8fde21733d1d4d384c1a4f63c
GregHernandez <gregoryarthur13@gmail.com>
2020-01-18 23:07:05 -0600

    Update router-auth.js
    - Redirect a logged user to home page

--- src/boot/router-auth.js
+++ src/boot/router-auth.js
@@ -1,9 +1,12 @@
+// import token service
 import { isTokenValid } from '../services/user'
 
 export default ({ router }) => {
   router.beforeEach((to, from, next) => {
+    // Call verify token function
+    let valid = isTokenValid()
+    // check if router requires auth to enter
     if (to.matched.some(record => record.meta.requiresAuth)) {
-      let valid = isTokenValid()
       if (valid === false) {
         next({
           path: '/login',
@@ -14,6 +17,12 @@ export default ({ router }) => {
       }
     } else {
       next()
+    }
+    // Redirect a logged user to home page
+    if (valid) {
+      if (to.path === '/login') {
+        next({ path: '/' })
+      }
     }
   })
 }
