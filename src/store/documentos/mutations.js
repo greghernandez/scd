b195cb33d6834dfd4c8206338e9f6b4518094ebb
GregHernandez <gregoryarthur13@gmail.com>
2020-01-27 21:01:40 -0600

    Update points when user upload files and comment mutations

--- src/store/documentos/mutations.js
+++ src/store/documentos/mutations.js
@@ -1,38 +1,43 @@
-// Set
+// Set loaded documents in docsection componet
 export function setDocumentos (state, documentos) {
   state.documentos = documentos
 }
-
+// Change loading state if the user is uploading documents
 export function isUploading (state) {
   state.isLoading = !state.isLoading
 }
-
+// Change loading state if the user is downloading documents
 export function changeDownloadState (state) {
   state.isDownloading = !state.isDownloading
 }
-
+// Add documents to store when user upload files
 export function addDocuments (state, documentos) {
   console.log('Payload', documentos)
   documentos.filter((item) => {
     state.documentos.push(item)
   })
 }
+// Stablish user total points
 export function setTotalPoints (state, points) {
   state.totalPoints = points
 }
+// Stablish displayed categories
 export function addCategories (state, categories) {
   state.categorias = categories
 }
+// Add card points to state
 export function addPoints (state, payload) {
   state.cardPoints.push(payload)
 }
+// reset category points
 export function resetCatPoints (state) {
   state.cardPoints = []
 }
-
+// Update points (total and cards) depending on user action
 export function updatePoints (state, payload) {
   console.log('UpdatePoints', payload)
   switch (payload.mode) {
+    // When a document is deleted
     case 'delete':
       // Reset deleted document points to totalPoints state
       state.totalPoints -= (payload.points * payload.deletedCount)
@@ -40,6 +45,17 @@ export function updatePoints (state, payload) {
       for (let index = 0; index < state.cardPoints.length; index++) {
         if (state.cardPoints[index].id === payload.catId) {
           state.cardPoints[index].totalValue -= (payload.points * payload.deletedCount)
+        }
+      }
+      break
+    // When a document is upload
+    case 'upload':
+      // Reset deleted document points to totalPoints state
+      state.totalPoints += (payload.points * payload.deletedCount)
+      // Subtract points to document category
+      for (let index = 0; index < state.cardPoints.length; index++) {
+        if (state.cardPoints[index].id === payload.catId) {
+          state.cardPoints[index].totalValue += (payload.points * payload.deletedCount)
         }
       }
       break
